98102259208528d2f10c12ba370e6842dc74a111
Félix Prado <felpragar@alum.us.es>
2015-11-23 00:32:15 +0100

    Assignment 2 (fixed)

--- cachematrix.R
+++ cachematrix.R
@@ -1,4 +1,4 @@
-get <- function() x## Put comments here that give an overall description of what your
+## Put comments here that give an overall description of what your
 ## functions do
 
 ## Write a short comment describing this function
